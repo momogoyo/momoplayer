0c938ec1220cb434e72df4cdec33d92d82586011
momogoyo <metheduxk@gmail.com>
2023-11-15 23:14:36 +0900

    ✨ 미디어 로드 처리

--- packages/core/src/components/Media.tsx
+++ packages/core/src/components/Media.tsx
@@ -8,8 +8,30 @@ export const Media = ({
   const mediaRef = useRef<HTMLMediaElement | null>(null)
   const [mediaType, setMediaType] = useState<string>('video')
 
+  const loadSource = () => {
+    if (!source.endsWith('.m3u8') || !source.endsWith('.mpd')) {
+      new Promise((resolve, reject) => {
+        mediaRef.current.load()
+        mediaRef.current.onloadeddata = resolve
+        mediaRef.current.onerror = reject
+      })
+    } else if (source.endsWith('.m3u8')) {
+      // hls.js 관련 load 로직
+      console.log('hls')
+    } else if (source.endsWith('.mpd')) {
+      // dash.js 관련 load 로직
+      console.log('dash')
+    } else {
+      new Error('지원하지 않는 타입 입니다.')
+    }
+  }
+
   useEffect(() => {
     setMediaType(checkMediaType(source))
+
+    if (mediaRef.current) {
+      loadSource()
+    }
   }, [source])
 
   return (
